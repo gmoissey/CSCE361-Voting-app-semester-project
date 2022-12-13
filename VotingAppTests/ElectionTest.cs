7be2898945c36e3cd1486b42be9b3c6345b53bd3
gmoissey <gmoisseyev2@huskers.unl.edu>
2022-12-12 18:34:22 -0600

    Person controller tests done

--- VotingAppTests/ElectionTest.cs
+++ VotingAppTests/ElectionTest.cs
@@ -13,13 +13,29 @@ public class ElectionTest : IClassFixture<TestDatabaseFixture>
 
     // xunit test to test the method GetElection in ElectionController.cs
     [Fact]
-    public async Task GetElectionTest()
+    public async Task GetElectionShouldNotReturNull()
     {
         // Arrange
         var controller = new ElectionController(Fixture.CreateContext());
 
         // Act
         var result = await controller.GetElection(1);
+
+        // Assert
+        var viewResult = Assert.IsType<ActionResult<Election>>(result);
+        var model = Assert.IsAssignableFrom<Election>(viewResult.Value);
+        Assert.NotNull(model);
+    }
+
+    [Fact]
+    public async Task PostElectionShouldNotReturNull()
+    {
+        // Arrange
+        var controller = new ElectionController(Fixture.CreateContext());
+
+        // Act
+        var result = await controller.PostElection(new Election { ID = 3, Candidate1Username = "user3.1", Candidate2Username = "user3.2", Title = "Election3", EndDate = new System.DateTime(2023, 12, 31) });
+        result = await controller.GetElection(1);
 
         // Assert
         var viewResult = Assert.IsType<ActionResult<Election>>(result);
--- VotingAppTests/TestDatabaseFixture.cs
+++ VotingAppTests/TestDatabaseFixture.cs
@@ -24,8 +24,24 @@ namespace VotingAppTests
                         context.Database.EnsureCreated();
 
                         context.AddRange(
-                            new Election { ID = 1, Candidate1Username = "user1.1", Candidate2Username = "user1.2", Title = "Election1", EndDate = new System.DateTime(2021, 12, 31) },
-                            new Election { ID = 2, Candidate1Username = "user2.1", Candidate2Username = "user2.1", Title = "Election2", EndDate = new System.DateTime(2022, 12, 31) });
+                            new Person { Username = "user1", PasswordHash = "password1", FirstName = "First1", LastName = "Last1", Party = "Party1" },
+                            new Person { Username = "user2", PasswordHash = "password2", FirstName = "First2", LastName = "Last2", Party = "Party2" },
+                            new Person { Username = "user3", PasswordHash = "password3", FirstName = "First3", LastName = "Last3", Party = "Party3"},
+                            new Person { Username = "user4", PasswordHash = "password4", FirstName = "First4", LastName = "Last4", Party = "Party4" },
+                            new Person { Username = "user5", PasswordHash = "password5", FirstName = "First5", LastName = "Last5", Party = "Party5" }
+                        );
+
+                        context.AddRange(
+                            new Election { ID = 1, Candidate1Username = "user1", Candidate2Username = "user2", Title = "Election1", EndDate = new System.DateTime(2021, 12, 31) },
+                            new Election { ID = 2, Candidate1Username = "user3", Candidate2Username = "user4", Title = "Election2", EndDate = new System.DateTime(2022, 12, 31) }
+                        );                        
+
+                        context.AddRange(
+                            new Votes {VoteID = 1, ElectionId = 1, VoterUsername = "user1", Vote = 1},
+                            new Votes {VoteID = 2, ElectionId = 1, VoterUsername = "user2", Vote = 2},
+                            new Votes {VoteID = 3, ElectionId = 2, VoterUsername = "user3", Vote = 1}
+                        );
+
                         context.SaveChanges();
                     }
 
